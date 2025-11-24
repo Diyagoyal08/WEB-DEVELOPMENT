aec4104f46dc35011ded76a7ebc5386d520d97aa
diya goyal <diya.goyal1607@gmail.com>
2025-11-24 23:39:48 +0530

    starting tic tac toe

--- testing/test.js
+++ testing/test.js
@@ -1,29 +1,14 @@
-const list = document.querySelector("ul");
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-const userchoice = document.getElementById("user-choice");
-const computerchoice = document.getElementById("computer-choice");
-const resultdisplay = document.getElementById("result");
-const choices = document.querySelectorAll("button");
-button.addEventListener("click", (event) => {
-  event.preventDefault();
+ function makeAdding (firstNumber) {
+  // "first" is scoped within the makeAdding function
+  const first = firstNumber;
+  return function resulting (secondNumber) {
+    // "second" is scoped within the resulting function
+    const second = secondNumber;
+    return first + second;
+  }
+}
+// but we've not seen an example of a "function"
+// being returned, thus far - how do we use it?
 
-  const myItem = input.value;
-  input.value = "";
-
-  const listItem = document.createElement("li");
-  const listText = document.createElement("span");
-  const listBtn = document.createElement("button");
-
-  listItem.appendChild(listText);
-  listText.textContent = myItem;
-  listItem.appendChild(listBtn);
-  listBtn.textContent = "Delete";
-  list.appendChild(listItem);
-
-  listBtn.addEventListener("click", () => {
-    list.removeChild(listItem);
-  });
-
-  input.focus();
-});
+const add5 = makeAdding(5);
+console.log(add5(2)) // logs 7
