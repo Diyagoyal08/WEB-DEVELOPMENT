6cdbdd7210c3eb632a169cc664d2a126592ca7ff
diya goyal <diya.goyal1607@gmail.com>
2026-02-04 23:08:40 +0530

    doing js

--- todo/src/index.js
+++ todo/src/index.js
@@ -6,3 +6,32 @@ const addProjectBtn = document.querySelector(".add-project-btn");
 
 console.log(addTodoBtn);
 console.log(addProjectBtn);
+ addTodoBtn.addEventListener("click", () => {
+  console.log("Add Todo button clicked");
+});
+
+addProjectBtn.addEventListener("click", () => {
+  console.log("Add Project button clicked");
+});
+
+// ----- Todo Data Logic -----
+
+function createTodo(title, dueDate, priority) {
+  return {
+    id: Date.now(),       // unique id
+    title: title,
+    dueDate: dueDate,
+    priority: priority,
+    completed: false,
+  };
+}
+let todos = [];
+const testTodo = createTodo(
+  "Learn JavaScript",
+  "2026-02-10",
+  "High"
+);
+
+todos.push(testTodo);
+
+console.log(todos);
