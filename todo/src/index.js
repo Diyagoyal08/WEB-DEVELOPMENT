de2d71001cf005c6fbb2f0ac2447db7e36257c57
diya goyal <diya.goyal1607@gmail.com>
2026-02-04 06:57:50 +0530

    src added

---
+++ todo/src/index.js
@@ -0,0 +1,8 @@
+ console.log("Step 3: JS loaded");
+
+// Select important DOM elements
+const addTodoBtn = document.querySelector(".add-todo-btn");
+const addProjectBtn = document.querySelector(".add-project-btn");
+
+console.log(addTodoBtn);
+console.log(addProjectBtn);
